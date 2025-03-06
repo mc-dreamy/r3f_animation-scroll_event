08e497aaa2fcceb2dbf77f2958de88d33d8015df
Henry <dev.mcdreamy@gmail.com>
2025-03-06 21:47:46 +0200

    add: model and implement animation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import { Canvas, useFrame } from "@react-three/fiber"
 import { useGLTF, useAnimations, useScroll, ScrollControls, SoftShadows } from "@react-three/drei"
 import { EffectComposer, TiltShift2 } from "@react-three/postprocessing"
 
+function Model(props) {
+  const scroll = useScroll()
+  console.log("scroll: ", scroll)
+  const { nodes, materials, animations } = useGLTF("/jump-transformed.glb")
+  console.log("animations: ", animations)
+  const { ref, actions } = useAnimations(animations)
+  console.log("ref: ", ref)
+  console.log("actions: ", actions)
+  useEffect(() => void (actions.jump.reset().play().paused = true), [])
+  useFrame(() => (actions.jump.time = actions.jump.getClip().duration * scroll.offset))
+  return (
+    <group {...props} ref={ref}>
+      <primitive object={nodes.mixamorigHips} />
+      <skinnedMesh castShadow receiveShadow geometry={nodes.Ch03.geometry} material={materials.Ch03_Body} skeleton={nodes.Ch03.skeleton} />
+    </group>
+  )
+}
+
 export const App = () => (
   <Canvas shadows gl={{ antialias: false }} camera={{ position: [1, 0.5, 2.5], fov: 50 }}>
     <color attach="background" args={["#f0f0f0"]} />
     <fog attach="fog" args={["#f0f0f0", 0, 20]} />
     <ambientLight intensity={0.5} />
     <directionalLight intensity={2} position={[-5, 5, 5]} castShadow shadow-mapSize={2048} shadow-bias={-0.0001} />
-    
+    <ScrollControls damping={0.2} maxSpeed={0.5} pages={2}>
+      <Model position={[0, -1, 0]} rotation={[Math.PI / 2, 0, 0]} scale={0.01} />
+    </ScrollControls>
     <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1.01, 0]} receiveShadow>
       <planeBufferGeometry args={[10, 10, 1, 1]} />
       <shadowMaterial transparent opacity={0.75} />
